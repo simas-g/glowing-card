c2fb321769444f5847f4d2801f29c3c7a293eca3
Simas G <simas@simson.lt>
2025-03-24 17:13:30 +0200

    glowing cards

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+'use client';
+import Card from '@/components/Card'
+export default function Home() {
+  return (
+    <div 
+      className="w-screen z-[-10] h-screen bg-gray-800 relative pt-20 flex justify-center"
+    >
+      <Card></Card>
+    </div>
+  );
+}
---
+++ components/Card.jsx
@@ -0,0 +1,12 @@
+'use client'
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className="border-gradient">
+      
+    </div>
+  )
+}
+
+export default Card
